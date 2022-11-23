851a360f87afb3f8af709ad3f972a9f74f7c541b
FloriPro <floriprolohner@gmail.com>
2022-11-23 14:47:29 +0100

    samesite = secure

--- utilitys.ts
+++ utilitys.ts
@@ -169,7 +169,7 @@ function setCookie(name: string, value: string, days: number) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/;SameSite=Secure ";
 }
 function getCookie(name: string): string {
     var nameEQ = name + "=";
