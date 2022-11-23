6a12987950572ac42a5658829548d600ab04b4a7
FloriPro <floriprolohner@gmail.com>
2022-11-23 14:59:12 +0100

    test

--- out/utilitys.js
+++ out/utilitys.js
@@ -184,7 +184,7 @@ function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/;SameSite=None;Secure";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=None; Secure";
 }
 function getCookie(name) {
     var nameEQ = name + "=";
--- utilitys.ts
+++ utilitys.ts
@@ -169,7 +169,7 @@ function setCookie(name: string, value: string, days: number) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/;SameSite=None;Secure";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=None; Secure";
 }
 function getCookie(name: string): string {
     var nameEQ = name + "=";
